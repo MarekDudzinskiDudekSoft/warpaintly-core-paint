75e006711ad37597cb4625c457744e98be225d46
marek <marek.patryk.dudzinski@gmail.com>
2025-01-05 19:16:11 +0100

    Init

---
+++ src/test/java/com/warpaintly/corepaint/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warpaintly.corepaint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
